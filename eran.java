bb0fbd51ec5617c047169018b12faff836b82007
erankimchi1 <71453515+erankimchi1@users.noreply.github.com>
2022-11-13 23:25:08 +0200

    updated program

--- eran.java
+++ eran.java
@@ -1,7 +1,7 @@
 public class eran{
 	public static void main(String[]args){
 		for(int i=1;i<=10;i++){
-			System.out.println("Eran number "+i);
+			System.out.println("Eran New number "+i);
 		}
 	}
 }
